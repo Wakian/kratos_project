f887d3a284bdc4db96911f3e32b8730f0a816a9c
Wakian <ianwaki2602@gmail.com>
2024-04-17 17:47:01 -0300

    added connection to NodeRed

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <esp32-hal-gpio.h>
 #include <Arduino.h>
 #include <ESP32Servo.h>
+#include <PubSubClient.h>
+#include <WiFi.h>
+
+Servo servo1;
+Servo servo2;
+
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+const char* mqtt_server = "broker.hivemq.com";// MQTT broker
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 #define led1   14
 #define led2   12
@@ -11,19 +23,22 @@
 #define lamp2  21
 #define lamp3  19
 
-#define door1  18 
-#define door2   5 
+#define door1  18
+#define door2   5
 
 #define TRIG   17
 #define ECHO   16
 
 #define button 26
 
-Servo servo1;
-Servo servo2;
+int mode = 0;
+// int tof = readTOF();
+int pos1 = 0;
+int pos2 = 0;
+long lastMsg = 0;
+char msg[50];
+int value = 0;
 
-int mode =     0;
-int tof = readTOF();
 
 void setup() {
   pinMode(led1,   OUTPUT);
@@ -41,28 +56,24 @@ void setup() {
   pinMode(TRIG,   OUTPUT);
   pinMode(ECHO,   INPUT);
 
+  pinMode(button, INPUT_PULLUP);
+
   servo1.attach(18);
   servo2.attach(5);
 
   Serial.begin(115200);
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
 void loop() {
-  switch (mode)
-  {
-  case 0: //modo normal / vai esperar msg do mqtt ou input do botoes para ligar os atuadores /
-    tof = readTOF();
-
-    break;
-  case 1: //modo cinema / desliga todas as luzes, tranca todas as portas e o led strip da sala de cinema liga em amarelo /
-    tof = readTOF();
-    
-    break;
-
-  case 2: //modo lockdown / tranca todas as portas liga todas as luzes e pisca os dois led strip em vermelho /
-    
-    break;
+  if (!client.connected()) {
+    reconnect();
   }
+  client.loop(); 
+ delay(100);
 }
 
 int readTOF(){
@@ -74,3 +85,79 @@ int readTOF(){
 
   return duration/58;
 }
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+   String str;
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+     str+=((char)payload[i]);  
+  }
+    Serial.print(str);
+if (topic ="ian_kratos")  /// esp32 subscribe topic   
+    Serial.print(" ");
+    split(str,' ');
+    if(str[0] == 1){
+      int status = str[1].stoi();   
+      int pos = map(status, 1, 100, 0, 180);
+      Serial.println(pos);
+      servo1.write(pos);
+      delay(15);
+    }else{
+      int status = str[1].stoi();   
+      int pos = map(status, 1, 100, 0, 180);
+      Serial.println(pos);
+      servo2.write(pos);
+      delay(15);
+    }
+     
+ }
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESPClient")) {
+      Serial.println("connected");
+      client.subscribe("ian_kratos");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds"); 
+      delay(5000);
+    }
+  }
+}
+
+void split (String str, char seperator)  
+{  
+  int currIndex = 0, i = 0;  
+  int startIndex = 0, endIndex = 0;  
+  while (i <= len(str)){
+    if (str[i] == seperator || i == len(str)){  
+      endIndex = i;  
+      String subStr = "";  
+      subStr.append(str, startIndex, endIndex - startIndex);  
+      strings[currIndex] = subStr;  
+      currIndex += 1;  
+      startIndex = endIndex + 1;  
+    }  
+    i++;  
+  }     
+}  
