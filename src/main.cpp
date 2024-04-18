a449d7cbd8a5b15be9677c9fddca8ef65b5e88d2
Wakian <ianwaki2602@gmail.com>
2024-04-18 15:26:05 -0300

    added led and lamp control

--- src/main.cpp
+++ src/main.cpp
@@ -74,7 +74,8 @@ void loop() {
     reconnect();
   }
   client.loop(); 
- delay(100);
+  delay(100);
+
 }
 
 int readTOF(){
@@ -115,7 +116,29 @@ void callback(char* topic, byte* payload, unsigned int length) {
 if (topic ="ian_kratos")  /// esp32 subscribe topic   
     Serial.print(" ");
 
-    if(str[0] == '1'){
+    if(str[0] == '0'){ //leds
+      if(str[1] == '1'){ //led 1
+        if(str[2] == '0'){digitalWrite(led1,LOW);} //off
+        else{digitalWrite(led1,HIGH);} //on
+      }
+
+      else if(str[1] == '2'){ //led 2
+        if(str[2] == '0'){digitalWrite(led2,LOW);} //off
+        else{digitalWrite(led2,HIGH);} //on
+      }
+
+      else if(str[1] == '3'){ //led 3
+        if(str[2] == '0'){digitalWrite(led3,LOW);} //off
+        else{digitalWrite(led3,HIGH);} //on
+      }
+
+      else if(str[1] == '4'){ //led 4
+        if(str[2] == '0'){digitalWrite(led4,LOW);} //off
+        else{digitalWrite(led4,HIGH);} //on
+      }
+    }
+
+    else if(str[0] == '1'){ //servo 1
       if(str[1] == '1'){ //servo 1 abra a porta
         Serial.println("porta da frente aberta!");
         servo1.write(90); 
@@ -123,15 +146,36 @@ if (topic ="ian_kratos")  /// esp32 subscribe topic
         Serial.println("porta da frente fechada!");
         servo1.write(0);
       }
-    }else if (str[0] == '2'){
+    }
+    
+    else if (str[0] == '2'){ // servo 2
       if(str[1] == '1'){ //servo 2 abra a porta
         Serial.println("porta de trás aberta!");
-        servo1.write(90); 
+        servo2.write(90); 
       }else if (str[1] == '2'){ //servo 2 feche a porta
         Serial.println("porta de trás fechada!");
-        servo1.write(0);
+        servo2.write(0);
       }
-    }     
+    }
+
+    else if(str[0] == '3'){ //lampadas
+      if(str[1] == '1'){ //lamp 1
+        if(str[2] == '0'){digitalWrite(lamp1,LOW);}
+        else{digitalWrite(lamp1,HIGH);}
+      }
+      else if (str[1] == '2'){ //lamp 2
+        if(str[2] == '0'){digitalWrite(lamp2,LOW);}
+        else{digitalWrite(lamp2,HIGH);}
+      }
+      else if (str[1] == '3'){
+        if(str[2] == '0'){digitalWrite(lamp3,LOW);}
+        else{digitalWrite(lamp3,HIGH);}        
+      }      
+    } 
+
+    else if(str[0] == '4'){ //ultra som
+
+    }  
  }
 
 void reconnect() {
